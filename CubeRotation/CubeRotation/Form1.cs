8890437289e4269f1a8a6b2dec75acadabd24944
Debechelo <90967633+Debechelo@users.noreply.github.com>
2022-02-19 18:35:41 +0300

    first commit

---
+++ CubeRotation/CubeRotation/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpGL;
+using SharpGL.SceneGraph.Assets;
+
+namespace CubeRotation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        float rquad = 0;
+
+        private void openGLControl1_OpenGLDraw(object sender, SharpGL.RenderEventArgs args)
+        {
+            
+
+            OpenGL gl = this.openGLControl1.OpenGL;
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+            gl.LoadIdentity();
+
+            gl.Translate(0.0f, 0.0f, -5.0f);
+
+            gl.Rotate(rquad, 0.0f, 1.0f, 0.0f);
+            gl.Begin(OpenGL.GL_QUADS);
+
+            gl.Color(0.0f, 1.0f, 0.0f);
+            gl.Vertex(1.0f, 1.0f, 1.0f);
+            gl.Vertex(-1.0f, 1.0f, 1.0f);
+            gl.Vertex(-1.0f, -1.0f, 1.0f);
+            gl.Vertex(1.0f, -1.0f, 1.0f);
+
+            gl.Color(1.0f, 1.0f, 0.0f);
+            gl.Vertex(1.0f, 1.0f, 1.0f);
+            gl.Vertex(1.0f, -1.0f, 1.0f);
+            gl.Vertex(1.0f, -1.0f, -1.0f);
+            gl.Vertex(1.0f, 1.0f, -1.0f);
+
+            gl.Color(1.0f, 0.0f, 1.0f);
+            gl.Vertex(1.0f, 1.0f, 1.0f);
+            gl.Vertex(-1.0f, 1.0f, 1.0f);
+            gl.Vertex(-1.0f, 1.0f, -1.0f);
+            gl.Vertex(1.0f, 1.0f, -1.0f);
+
+            gl.Color(0.0f, 1.0f, 1.0f);
+            gl.Vertex(1.0f, 1.0f, -1.0f);
+            gl.Vertex(-1.0f, 1.0f, -1.0f);
+            gl.Vertex(-1.0f, -1.0f, -1.0f);
+            gl.Vertex(1.0f, -1.0f, -1.0f);
+
+            gl.Color(1.0f, 0.0f, 0.0f);
+            gl.Vertex(-1.0f, 1.0f, 1.0f);
+            gl.Vertex(-1.0f, -1.0f, 1.0f);
+            gl.Vertex(-1.0f, -1.0f, -1.0f);
+            gl.Vertex(-1.0f, 1.0f, -1.0f);
+
+            gl.Color(0.0f, 0.0f, 1.0f);
+            gl.Vertex(1.0f, -1.0f, 1.0f);
+            gl.Vertex(-1.0f, -1.0f, 1.0f);
+            gl.Vertex(-1.0f, -1.0f, -1.0f);
+            gl.Vertex(1.0f, -1.0f, -1.0f);
+            gl.End();
+            gl.Flush();
+            rquad += 2.0f;
+        }
+    }
+}
